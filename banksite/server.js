0116e63610e8f1b8e05357772d42352ae77fe491
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-02 16:29:37 +0530

    Create the simple express server to serve the frontend.html for banksite

--- banksite/server.js
+++ banksite/server.js
@@ -1,1 +1,13 @@
-console.log("SERVER");
+const express = require('express');
+const fs = require('fs');
+
+const PORT = 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const page = fs.readFileSync('frontend.html', 'utf-8');
+    res.send(page);
+});
+
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
