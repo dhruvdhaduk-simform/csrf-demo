1ef45db6ba6854086ab88e66135c03a9689d7ac1
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-03 12:05:26 +0530

    Add logic in banksite v2 server to authorize based on csrf token

--- banksite_v2/server.js
+++ banksite_v2/server.js
@@ -9,8 +9,17 @@ const PORT = 8000;
 
 const app = express();
 
+const csrfTokens = new Map();
+const TOKEN_EXPIRY = 30 * 1000;
+
 function generateCsrfToken() {
-    const token =crypto.randomBytes(20).toString('hex');
+    const token = crypto.randomBytes(20).toString('hex');
+    csrfTokens.set(token, Date.now());
+
+    setTimeout(() => {
+        csrfTokens.delete(token);
+    }, TOKEN_EXPIRY);
+
     return token;
 }
 
@@ -26,13 +35,22 @@ app.get('/', (req, res) => {
 });
 
 app.post('/transfer', (req, res) => {
-    console.log(req.body);
     const senderName = req.body['sender-name'];
     const receiverName = req.body['receiver-name'];
     const amount = req.body.amount;
     const csrfToken = req.headers['x-csrf-token'];
 
-    const msg = `$${amount} was transferred from ${senderName} to ${receiverName}. TOKEN : ${csrfToken}`;
+    if (!csrfToken) {
+        return res.send('Unauthorized Request. No CSRF token found.');
+    }
+
+    if (!csrfTokens.has(csrfToken)) {
+        return res.send('Invalid or expired CSRF token.');
+    }
+
+    csrfTokens.delete(csrfToken);
+
+    const msg = `$${amount} was transferred from ${senderName} to ${receiverName}.`;
     console.log(msg);
     res.send(msg);
 });
