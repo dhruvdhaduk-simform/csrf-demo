3646c1c54776cc8931979fb5145166bee83cbf5a
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-03 10:48:47 +0530

    Add the logic to simulate the transaction in bank site

--- banksite/server.js
+++ banksite/server.js
@@ -1,3 +1,4 @@
+const exp = require('constants');
 const express = require('express');
 const fs = require('fs');
 
@@ -5,13 +6,18 @@ const PORT = 8000;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     const page = fs.readFileSync('frontend.html', 'utf-8');
     res.send(page);
 });
 
 app.post('/transfer', (req, res) => {
-    res.send('Transfer API is working.');
+    const senderName = req.body.senderName;
+    const receiverName = req.body.receiverName;
+    const amount = req.body.amount;
+    res.send(`$${amount} was transferred from ${senderName} to ${receiverName}`);
 });
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
